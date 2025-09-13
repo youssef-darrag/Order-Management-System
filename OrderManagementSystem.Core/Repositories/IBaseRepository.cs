48d1d28943444ee7ec38f31e1f249778e07cdd4b
Youssef-Darrag <darrag904@gmail.com>
2025-09-13 22:24:49 +0300

    Add BaseRepository.

---
+++ OrderManagementSystem.Core/Repositories/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderManagementSystem.Core.Repositories
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+    }
+}
